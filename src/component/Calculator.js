d2fe538141ffba6b526b4033017ee1ec7e25b223
QueenterJuma <jumahqueenter@gmail.com>
2023-03-21 16:54:38 +0300

    Add className orenge in Calculator.js file

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -2,32 +2,42 @@ import React from 'react';
 import './style/Calculator.css';
 
 const Calculator = () => (
-  <div className="calculator">
+  <div id="calculator">
     <div className="display">
       <span>0</span>
     </div>
-    <div className="keypad">
+    <div id="keypad">
       <button type="button">AC</button>
       <button type="button">+/-</button>
       <button type="button">%</button>
-      <button type="button">/</button>
+      <button type="button" className="orange">
+        /
+      </button>
       <button type="button">7</button>
       <button type="button">8</button>
       <button type="button">9</button>
-      <button type="button">*</button>
+      <button type="button" className="orange">
+        *
+      </button>
       <button type="button">4</button>
       <button type="button">5</button>
       <button type="button">6</button>
-      <button type="button">-</button>
+      <button type="button" className="orange">
+        -
+      </button>
       <button type="button">1</button>
       <button type="button">2</button>
       <button type="button">3</button>
-      <button type="button">+</button>
+      <button type="button" className="orange">
+        +
+      </button>
       <button type="button" className="zero">
         0
       </button>
       <button type="button">.</button>
-      <button type="button">=</button>
+      <button type="button" className="orange">
+        =
+      </button>
     </div>
   </div>
 );
