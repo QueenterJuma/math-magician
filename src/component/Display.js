fc6ad58d53739f3454ce993e5afdc6bcf45081dc
QueenterJuma <jumahqueenter@gmail.com>
2023-03-22 20:23:28 +0300

    Add update in Display.js

--- src/component/Display.js
+++ src/component/Display.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Display = () => (
   <div className="display">
     <span>0</span>
   </div>
 );
+
+// const NextSolution = ({ solution }) => <p id="next-solution">{solution}</p>;
+  
+// NextResult.propTypes = { solution: PropTypes.string.isRequired };
+
+// export default NextSolution;
 
 export default Display;
