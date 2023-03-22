c648232124f15d4d228d7cc8db0cc659387ee5f0
QueenterJuma <jumahqueenter@gmail.com>
2023-03-22 20:22:54 +0300

    Add update in calculator.js

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style/Calculator.css';
 import Display from './Display';
+import calculate from '../Logic/calculate';
 
-const Calculator = () => (
-  <div id="calculator">
-    <Display />
-    <div id="keypad">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="orange">
-        ÷
-      </button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="orange">
-        *
-      </button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="orange">
-        -
-      </button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="orange">
-        +
-      </button>
-      <button type="button" className="zero">
-        0
-      </button>
-      <button type="button">.</button>
-      <button type="button" className="orange">
-        =
-      </button>
+const Calculator = () => {
+  // const [solution, setSolution] = useState(0);
+  //  const handleClick = (e) => {
+  //    setSolution((obj) => calculate(obj, e.target.innerText));
+  //  };
+  return (
+    <div id="calculator">
+      <Display />
+      <div id="keypad">
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="orange">
+          ÷
+        </button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="orange">
+          *
+        </button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="orange">
+          -
+        </button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="orange">
+          +
+        </button>
+        <button type="button" className="zero">
+          0
+        </button>
+        <button type="button">.</button>
+        <button type="button" className="orange">
+          =
+        </button>
+      </div>
     </div>
-  </div>
-);
+  ); 
+};
 
 export default Calculator;
