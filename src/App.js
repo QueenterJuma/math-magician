464f1fa244b7712295f60f4dafe1a7c718d5b625
QueenterJuma <jumahqueenter@gmail.com>
2023-03-24 12:21:32 +0300

    Add FetchQuote and className in App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './App.css';
 import Calculator from './component/Calculator';
+import FetchQuote from './component/quoteComponent';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <div className="fetchQuotes">
+        <FetchQuote />
+      </div>
+      <div className="calculatorContainer">
+        <Calculator />
+      </div>
     </div>
   );
 }
