46e0497a1943355fb1216ffee0f886241fc0c6e2
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-29 17:07:29 +0300

    Update test files

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,24 @@
+import calculate from '../Logic/calculate'
+
+describe('test calculate function', () => {
+    test('it should return sum output', () => {
+      const obj = { total: '1', next: '2', operation: '+' };
+      const buttonName = '=';
+      const output = calculate(obj, buttonName);
+      expect(output.total).toBe('3');
+    });
+  
+    test('it should return sum output', () => {
+      const obj = { total: '6', next: '2', operation: '+' };
+      const buttonName = '=';
+      const output = calculate(obj, buttonName);
+      expect(output.total).toBe('8');
+    });
+  
+    test('it should return substract output', () => {
+      const obj = { total: '8', next: '2', operation: '-' };
+      const buttonName = '=';
+      const output = calculate(obj, buttonName);
+      expect(output.total).toBe('6');
+    });
+  });
